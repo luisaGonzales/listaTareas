09c355a98fd93b82f5967c874796051d1c597865
luisaGonzales <luisagc711@gmail.com>
2017-07-25 16:00:42 -0500

    3. JS clase lista

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -15,3 +15,23 @@ function Tarea (id, title, completed ){
     return tarea;
   }
 }
+
+function ListaCompleta () {
+  this.tareas = [];
+  this.agregar = function (tarea){
+    this.tareas.push(tarea);
+  }
+  this.mostrar = function(element){
+    var stringHtml = "";
+    element.innerHTML = stringHtml;
+    for (var i in this.tareas){
+      var task = this.tareas[i];
+      if(task.completed){
+        stringHtml += "<li style = background-color:yellow>" + task.toHTML() + "</li>";
+      } else {
+        stringHtml += "<li>" + task.toHTML() + "</li>";
+      }
+    }
+    element.innerHTML = stringHtml;
+  }
+}
